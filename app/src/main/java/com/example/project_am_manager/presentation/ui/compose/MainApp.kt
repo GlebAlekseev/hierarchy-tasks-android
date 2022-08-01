0a1127245582078fef552e1414acecd4f239e739
GlebAlekseev <glebalekseevjk@gmail.com>
2022-08-01 03:04:53 +0300

    refactoring

--- app/src/main/java/com/example/project_am_manager/presentation/ui/compose/MainApp.kt
+++ app/src/main/java/com/example/project_am_manager/presentation/ui/compose/MainApp.kt
@@ -1,11 +1,5 @@
-@file:OptIn(
-    ExperimentalAnimationApi::class, ExperimentalAnimationApi::class,
-    ExperimentalAnimationApi::class
-)
-
 package com.example.project_am_manager.presentation.ui.compose
 
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import com.example.project_am_manager.presentation.ui.compose.appcontent.MainAppContent
 import com.example.project_am_manager.presentation.viewmodel.MainViewModel
